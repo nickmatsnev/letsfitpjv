8600f8d4f5805e36df064408fb759ceb93283260
Nikita Matsnev <matsnnik@fit.cvut.cz>
2021-02-07 20:40:47 +0100

    added jar

--- src/sample/MegaController.java
+++ src/sample/MegaController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
